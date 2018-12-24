27947f1b3643ab69423a8296ba8246a1442e3d51
Rimlin <Rimlin@gmail.com>
2018-12-24 18:46:47 +0200

    nerw2

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-
         Scanner sc = new Scanner(System.in);
 
-        if (sc.hasNextInt())
-        {
-            int m = sc.nextInt();
-            {
-                int d = 1; int c = 1;
-                d = m % 2;
-                if (d == 0) //a = a + 1;
-
-                    System.out.println("true");
-                else System.out.println("false");
+        if (sc.hasNextInt()) {
+            if (sc.nextInt() % 2 == 0) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
             }
         }
     }
